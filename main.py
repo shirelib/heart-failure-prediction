05e1905099227c406661ed2333eb6162b01feaad
shirelib <103411746+shirelib@users.noreply.github.com>
2023-12-22 23:06:04 +0200

    Adding SVM with RBF kernel
    Adding SVM with RBF kernel & a gridsearch performance plot

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, RepeatedKFold, GridSearchCV, cross_val_score
 from sklearn.linear_model import ElasticNet, LogisticRegression
+from sklearn.svm import SVC
 
 df = pd.read_csv('heart_failure_clinical_records_dataset.csv')
 pd.set_option('display.max_columns', None)
@@ -50,8 +51,44 @@ plt.suptitle('Pairplot of Predictors vs Target', y=1.02)
 #plt.show()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-#param_grid = {'alpha': [0.01, 0.1, 0.5, 1, 2],
-#              'l1_ratio': [0.1, 0.3, 0.5, 0.8, 1.0]}
+
+# Standardize features
+scaler = StandardScaler().fit(X_train)
+X_train_scaled = scaler.transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 200, 350, 500],
+              'gamma': [0.001, 0.01, 0.1, 1, 10, 100]}
+
+svm = SVC(kernel='rbf')
+clf = GridSearchCV(svm, param_grid, scoring='f1') #['accuracy', 'precision', 'recall', 'f1']
+clf.fit(X_train_scaled, y_train)
+print(pd.DataFrame(clf.cv_results_))
+print("clf.best_params_\n", clf.best_params_)
+print("clf.best_score_\n", clf.best_score_)
+best_svm = clf.best_estimator_
+
+# CV models plot
+results = clf.cv_results_
+C_values = results['param_C'].data
+gamma_values = results['param_gamma'].data
+metric_scores = results['mean_test_score']
+
+plt.figure(figsize=(8, 6))
+for gamma_value in sorted(set(gamma_values)):
+    gamma_indices = [i for i, val in enumerate(gamma_values) if val == gamma_value]
+    plt.plot(C_values[gamma_indices], metric_scores[gamma_indices], label=f"gamma={gamma_value}", linewidth=2)
+
+plt.xlabel("C parameter")
+plt.ylabel("F1 Score")  # Adjust label based on metric
+plt.title("Hyperparameter Tuning Results")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+###
+
+
 
 #enet_mod = ElasticNet(random_state=42)
 #rkf = RepeatedKFold(n_splits=5, n_repeats=5, random_state=42)
